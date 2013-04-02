040d8a1cb3d4f5024d58a6e15cdbb9252f24340c
edokeh <edokeh@163.com>
2013-04-02 10:28:00 +0800

    whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,7 +24,7 @@ set :output, "~/vmshare/klog/log/whenever.log"
 #  command 'backup perform -t klog -c ~/klog/current/config/backup_config.rb'
 #end
 
-every :day, :at=>'15:00' do # Many shortcuts available: :hour, :day, :month, :year, :reboot
+every :day, :at=>'02:31' do # Many shortcuts available: :hour, :day, :month, :year, :reboot
   runner "Captcha.destroy_all"
   runner "Captcha.random_create(20)"
 end
