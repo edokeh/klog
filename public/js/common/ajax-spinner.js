e1ee7b5326b3483d8f9b99e63cd911612012bb5a
edokeh <edokeh@163.com>
2013-11-14 16:45:19 +0800

    体验更好的 Ajax Spinner

--- public/js/common/ajax-spinner.js
+++ public/js/common/ajax-spinner.js
@@ -3,15 +3,32 @@
  */
 angular.module('common').config(['$httpProvider', function($httpProvider) {
 
-    $httpProvider.interceptors.push(['$rootScope', '$q', function($rootScope, $q) {
+    $httpProvider.interceptors.push(['$rootScope', '$q', '$timeout', function($rootScope, $q, $timeout) {
+        var lastTimeout;
+        var requestQueue =[];
+        var SHOW_AFTER = 200;
+
         return {
             'request': function(config) {
-                $rootScope.ajaxing = true;
-                $rootScope.ajaxingMethod = config.method;
+                config.reqTimeout = $timeout(function() {
+                    $rootScope.ajaxing = true;
+                    $rootScope.ajaxingMethod = config.method;
+                }, SHOW_AFTER);
+                config.requestStartAt = new Date().getTime();
+                requestQueue.push(config);
+
                 return config || $q.when(config);
             },
             'response': function(response) {
-                $rootScope.ajaxing = false;
+                var config = response.config;
+                requestQueue = _.without(requestQueue, config);
+                $timeout.cancel(config.reqTimeout);
+                $timeout(function() {
+                    if(requestQueue.length === 0){
+                        $rootScope.ajaxing = false;
+                    }
+                }, config.requestStartAt + SHOW_AFTER + 700 - new Date().getTime());
+
                 return response || $q.when(response);
             }
         };
